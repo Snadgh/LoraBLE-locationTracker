6017ef2fa2aefb8cf214f23694c9292a0b5c25b7
snaedis <snaedisdaniels@gmail.com>
2024-04-29 11:17:19 +0200

    heltec lora code including LCD

---
+++ slave/LoRaWAN/heltec-lora-LCD/heltec-lora-LCD.ino
@@ -0,0 +1,231 @@
+/* Heltec Automation LoRaWAN communication example
+ *
+ * Function:
+ * 1. Upload node data to the server using the standard LoRaWAN protocol.
+ * 2. The network access status of LoRaWAN is displayed on the screen.
+ * 
+ * Description:
+ * 1. Communicate using LoRaWAN protocol.
+ * 
+ * HelTec AutoMation, Chengdu, China
+ * 成都惠利特自动化科技有限公司
+ * www.heltec.org
+
+ *
+ * this project also realess in GitHub:
+ * https://github.com/Heltec-Aaron-Lee/WiFi_Kit_series
+
+ * */
+
+#include "LoRaWan_APP.h"
+#include "HT_SSD1306Wire.h"
+#define VBAT_CTRL GPIO_NUM_37
+#define VBAT_ADC  GPIO_NUM_1
+/* OTAA para*/
+uint8_t devEui[] = { 0x60, 0x81, 0xF9, 0x54, 0xC4, 0x47, 0x8E, 0x50};
+uint8_t appEui[] = { 0x60, 0x81, 0xF9, 0x8F, 0x0D, 0x52, 0x3E, 0xDC};
+uint8_t appKey[] = { 0x99, 0x65, 0xBE, 0xA8, 0xF4, 0x0D, 0x92, 0x4A,
+                    0x43, 0x7E, 0x86, 0x76, 0x32, 0x89, 0xF3, 0x0D};
+
+ /* ABP para*/
+ uint8_t nwkSKey[] = { 0x0B, 0xD2, 0x2E, 0x3C, 0x93, 0xAA, 0x59, 0xF3, 0x67, 0x85, 0x82, 0xDE, 0x0D, 0x13, 0xBA, 0xBC };
+ uint8_t appSKey[] = { 0xDE, 0x29, 0x8F, 0xC4, 0x1D, 0x1C, 0x83, 0x2E, 0xCF, 0x5C, 0x96, 0xC2, 0x87, 0x73, 0xB0, 0x3B };
+ 
+ uint32_t devAddr =  ( uint32_t )0x01485B2B;
+
+
+const float min_voltage = 3.04;
+const float max_voltage = 4.26;
+const uint8_t scaled_voltage[100] = {
+  254, 242, 230, 227, 223, 219, 215, 213, 210, 207,
+  206, 202, 202, 200, 200, 199, 198, 198, 196, 196,
+  195, 195, 194, 192, 191, 188, 187, 185, 185, 185,
+  183, 182, 180, 179, 178, 175, 175, 174, 172, 171,
+  170, 169, 168, 166, 166, 165, 165, 164, 161, 161,
+  159, 158, 158, 157, 156, 155, 151, 148, 147, 145,
+  143, 142, 140, 140, 136, 132, 130, 130, 129, 126,
+  125, 124, 121, 120, 118, 116, 115, 114, 112, 112,
+  110, 110, 108, 106, 106, 104, 102, 101, 99, 97,
+  94, 90, 81, 80, 76, 73, 66, 52, 32, 7,
+};
+/*LoraWan channelsmask*/
+uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
+
+/*LoraWan region, select in arduino IDE tools*/
+LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
+
+/*LoraWan Class, Class A and Class C are supported*/
+DeviceClass_t  loraWanClass = CLASS_A;
+
+/*the application data transmission duty cycle.  value in [ms].*/
+uint32_t appTxDutyCycle = 15000;
+
+/*OTAA or ABP*/
+bool overTheAirActivation = true;
+
+/*ADR enable*/
+bool loraWanAdr = true;
+
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
+bool isTxConfirmed = true;
+
+/* Application port */
+uint8_t appPort = 2;
+/*!
+* Number of trials to transmit the frame, if the LoRaMAC layer did not
+* receive an acknowledgment. The MAC performs a datarate adaptation,
+* according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
+* to the following table:
+*
+* Transmission nb | Data Rate
+* ----------------|-----------
+* 1 (first)       | DR
+* 2               | DR
+* 3               | max(DR-1,0)
+* 4               | max(DR-1,0)
+* 5               | max(DR-2,0)
+* 6               | max(DR-2,0)
+* 7               | max(DR-3,0)
+* 8               | max(DR-3,0)
+*
+* Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
+* the datarate, in case the LoRaMAC layer did not receive an acknowledgment
+*/
+uint8_t confirmedNbTrials = 4;
+// uint8_t appData[255];
+
+SSD1306Wire  factory_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
+
+/* Prepares the payload of the frame */
+static void prepareTxFrame( uint8_t port )
+{
+	/*appData size is LORAWAN_APP_DATA_MAX_SIZE which is defined in "commissioning.h".
+	*appDataSize max value is LORAWAN_APP_DATA_MAX_SIZE.
+	*if enabled AT, don't modify LORAWAN_APP_DATA_MAX_SIZE, it may cause system hanging or failure.
+	*if disabled AT, LORAWAN_APP_DATA_MAX_SIZE can be modified, the max value is reference to lorawan region and SF.
+	*for example, if use REGION_CN470, 
+	*the max value for different DR can be found in MaxPayloadOfDatarateCN470 refer to DataratesCN470 and BandwidthsCN470 in "RegionCN470.h".
+	*/
+
+  
+  appDataSize = 13;
+  appData[0] = 0x00;
+  appData[1] = 0x2a;
+  appData[2] = 0x10;
+  appData[3] = 0x1a;
+  appData[4] = 0xfd;
+  appData[5] = 0x5e;
+  appData[6] = 0xcc;
+  appData[7] = 0x16;
+  appData[8] = 0x7e;
+  appData[9] = 0xdd;
+  appData[10] = 0x94;
+  appData[11] = 0xbb;
+  appData[12] = 0x80;
+}
+
+RTC_DATA_ATTR bool firstrun = true;
+
+void setup() {
+  Serial.begin(115200);
+  Mcu.begin(HELTEC_BOARD,SLOW_CLK_TPYE);
+  factory_display.init();
+  if(firstrun)
+  {
+    LoRaWAN.displayMcuInit();
+    firstrun = false;
+  }
+
+}
+
+float heltec_vbat() {
+  pinMode(VBAT_CTRL, OUTPUT);
+  digitalWrite(VBAT_CTRL, LOW);
+  delay(5);
+  float vbat = analogRead(VBAT_ADC) / 238.7;
+  // pulled up, no need to drive it
+  pinMode(VBAT_CTRL, INPUT);
+  return vbat;
+}
+
+int heltec_battery_percent(float vbat = -1) {
+  if (vbat == -1) {
+    vbat = heltec_vbat();
+  }
+  for (int n = 0; n < sizeof(scaled_voltage); n++) {
+    float step = (max_voltage - min_voltage) / 256;//0.004765; 1.22/256
+    
+    if (vbat > min_voltage + (step * scaled_voltage[n])) {
+      float result = min_voltage + (step * scaled_voltage[n]);
+      
+      return 100 - n;
+    }
+  }
+  return 0;
+}
+
+void loop()
+{
+  float voltage = heltec_vbat();
+
+  int voltage_percentage = heltec_battery_percent(voltage);
+
+  String strPercentage = String(voltage_percentage);
+
+  String strVoltage = String(voltage, 2);
+  Serial.println(strVoltage);
+  Serial.println(strPercentage);
+  factory_display.clear();
+  factory_display.drawString(0, 0, strVoltage + "V");
+  factory_display.drawString(0, 10, strPercentage + "%");
+  factory_display.display();
+  
+	switch( deviceState )
+	{
+    
+		case DEVICE_STATE_INIT:
+		{
+      #if(LORAWAN_DEVEUI_AUTO)
+			  LoRaWAN.generateDeveuiByChipID();
+      #endif
+			LoRaWAN.init(loraWanClass,loraWanRegion);
+			//both set join DR and DR when ADR off 
+			LoRaWAN.setDefaultDR(3);
+			break;
+		}
+		case DEVICE_STATE_JOIN:
+		{
+			LoRaWAN.displayJoining();
+			LoRaWAN.join();
+			break;
+		}
+		case DEVICE_STATE_SEND:
+		{
+			LoRaWAN.displaySending();
+			prepareTxFrame( appPort );
+			LoRaWAN.send();
+			deviceState = DEVICE_STATE_CYCLE;
+			break;
+		}
+		case DEVICE_STATE_CYCLE:
+		{
+			// Schedule next packet transmission
+			txDutyCycleTime = appTxDutyCycle + randr( -APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND );
+			LoRaWAN.cycle(txDutyCycleTime);
+			deviceState = DEVICE_STATE_SLEEP;
+			break;
+		}
+		case DEVICE_STATE_SLEEP:
+		{
+			LoRaWAN.displayAck();
+			LoRaWAN.sleep(loraWanClass);
+			break;
+		}
+		default:
+		{
+			deviceState = DEVICE_STATE_INIT;
+			break;
+		}
+	}
+}
